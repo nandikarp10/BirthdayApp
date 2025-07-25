553335fbe207fb6c81bb0e2fd061727d8047201e
Nandika Peddapuram <nandikarp10@gmail.com>
2025-07-25 13:42:55 -0500

    Delete Function

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -16,14 +16,20 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack{
-            List(friends){ friend in
-                HStack{
-                    Text(friend.name)
-                    Spacer()
-                    Text(friend.birthday, format:
-                            .dateTime.month(.wide).day().year())
-            }
-        }
+            
+            List {
+                    ForEach(friends) { friend in
+                        HStack {
+                            HStack {
+                                Text(friend.name)
+                                Spacer()
+                                Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteFriend)
+                }
+                
             .navigationTitle("Birthdays")
             .safeAreaInset(edge:.bottom){
                 VStack(alignment: .center, spacing: 20){
@@ -48,6 +54,14 @@ struct ContentView: View {
         }
         
     }
+    
+    func deleteFriend(at offsets: IndexSet){
+        for index in offsets{
+            let friendToDelete = friends[index]
+            context.delete(friendToDelete)
+        }
+    }
+    
 }
 
 #Preview {
